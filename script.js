3ebeb7d731c047d620358ad417f2aa7e4e7c7eea
Joe Santos <jaysantos012@gmail.com>
2020-11-13 16:09:34 -0500

    Finished most of the JS, Now adding localStoage to remember the data entered witht ehjs

---
+++ script.js
@@ -0,0 +1,39 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+let ticketPrice = +movieSelect.value;
+
+
+// Update total count
+
+
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const selectedSeatsCount = selectedSeats.length;
+
+    count.innerHTML = selectedSeatsCount;
+    total.innerHTML = selectedSeatsCount * ticketPrice;
+    
+}
+
+movieSelect.addEventListener('change', e => {
+    ticketPrice = +e.target.value;
+    updateSelectedCount();
+})
+
+
+
+
+
+container.addEventListener('click', e => {
+    if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')
+    ) {
+        e.target.classList.toggle('selected');
+
+        updateSelectedCount();
+    }
+});
