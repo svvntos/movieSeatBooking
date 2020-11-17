de58bf6bcb4a3a391a60b8f07ce7525ce21f4d48
Joe Santos <jaysantos012@gmail.com>
2020-11-16 19:48:54 -0500

    Finished the Js, the app is fully functional

--- script.js
+++ script.js
@@ -4,30 +4,50 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI();
+
 let ticketPrice = +movieSelect.value;
 
+function setMovieData(movieIndex, moviePrice) {
 
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
 // Update total count
-
-
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const seatsIndex = [...selectedSeats].map(function (seat) {
+        return [...seats].indexOf(seat)
+    });
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
     const selectedSeatsCount = selectedSeats.length;
 
     count.innerHTML = selectedSeatsCount;
     total.innerHTML = selectedSeatsCount * ticketPrice;
-    
+
+};
+
+function populateUI () {
+
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats')
+    );
+        
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        });
+    }
+
 }
 
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 })
-
-
-
-
 
 container.addEventListener('click', e => {
     if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')
@@ -37,3 +57,5 @@ container.addEventListener('click', e => {
         updateSelectedCount();
     }
 });
+
+updateSelectedCount();
